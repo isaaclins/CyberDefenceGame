12a2981db865dcad8caa3480bb18609b08f28362
Isaac Lins <contact@isaaclins.com>
2025-02-14 15:08:26 +0100

    remove Constants class as it is no longer needed

--- src/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package src.utils;
-
-
-public class Constants {
-    public static final String GAME_TITLE = "Deckbuilding Game";
-    public static final int FRAME_WIDTH = 800;
-    public static final int FRAME_HEIGHT = 600;
-}
